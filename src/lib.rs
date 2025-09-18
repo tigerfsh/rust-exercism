01a2c13e8ed28735e5e16a30ef070a6788c924d9
fushouhai <fushouhai@starmerx.com>
2025-09-18 11:36:13 +0800

    6. space age

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,62 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
         .collect()
 }
 
+#[derive(Debug)]
+pub struct CDuration {
+    seconds: u64,
+}
+
+impl From<u64> for CDuration {
+    fn from(s: u64) -> Self {
+        CDuration { seconds: s }
+    }
+}
+
+pub trait Planet {
+    // Orbital period in Earth years
+    const ORBITAL_PERIOD: f64;
+
+    fn years_during(d: &CDuration) -> f64 {
+        let earth_year_seconds = 31_557_600.0;
+        let seconds = d.seconds as f64;
+        seconds / (Self::ORBITAL_PERIOD * earth_year_seconds)
+    }
+}
+
+pub struct Mercury;
+pub struct Venus;
+pub struct Earth;
+pub struct Mars;
+pub struct Jupiter;
+pub struct Saturn;
+pub struct Uranus;
+pub struct Neptune;
+
+impl Planet for Mercury {
+    const ORBITAL_PERIOD: f64 = 0.2408467;
+}
+impl Planet for Venus {
+    const ORBITAL_PERIOD: f64 = 0.61519726;
+}
+impl Planet for Earth {
+    const ORBITAL_PERIOD: f64 = 1.0;
+}
+impl Planet for Mars {
+    const ORBITAL_PERIOD: f64 = 1.8808158;
+}
+impl Planet for Jupiter {
+    const ORBITAL_PERIOD: f64 = 11.862615;
+}
+impl Planet for Saturn {
+    const ORBITAL_PERIOD: f64 = 29.447498;
+}
+impl Planet for Uranus {
+    const ORBITAL_PERIOD: f64 = 84.016846;
+}
+impl Planet for Neptune {
+    const ORBITAL_PERIOD: f64 = 164.79132;
+}
+
 #[cfg(test)]
 mod anagram_tests {
     use super::anagrams_for;
