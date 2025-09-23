11e8af4238cd8ceffc9b50d57037146598b04153
fushouhai <fushouhai@starmerx.com>
2025-09-23 14:18:26 +0800

    21. matching-brackets

--- src/lib.rs
+++ src/lib.rs
@@ -571,6 +571,33 @@ pub fn raindrops(n: u32) -> String {
     result
 }
 
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut stack = Vec::new();
+    for c in string.chars() {
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+    stack.is_empty()
+}
+
+
 #[cfg(test)]
 mod raindrops_tests {
     use super::*;
