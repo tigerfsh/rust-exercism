e8bf23140ca860987a345a971121975ad36fc983
fushouhai <fushouhai@starmerx.com>
2025-09-23 13:44:16 +0800

    19. proverb

--- src/lib.rs
+++ src/lib.rs
@@ -528,24 +528,22 @@ pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     map
 }
 
-/*
-For want of a horseshoe nail, a kingdom was lost, or so the saying goes.
-
-Given a list of inputs, generate the relevant proverb. For example, given the list ["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"], you will output the full text of this proverbial rhyme:
-
-For want of a nail the shoe was lost.
-For want of a shoe the horse was lost.
-For want of a horse the rider was lost.
-For want of a rider the message was lost.
-For want of a message the battle was lost.
-For want of a battle the kingdom was lost.
-And all for the want of a nail.
-Note that the list of inputs may vary; your solution should be able to handle lists of arbitrary length and content. No line of the output text should be a static, unchanging string; all should vary according to the input given.
-
-*/
-
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    if list.is_empty() {
+        return String::new();
+    }
+    let mut lines = Vec::new();
+    for i in 0..list.len() - 1 {
+        lines.push(format!(
+            "For want of a {} the {} was lost.",
+            list[i], list[i + 1]
+        ));
+    }
+    lines.push(format!(
+        "And all for the want of a {}.",
+        list[0]
+    ));
+    lines.join("\n")
 }
 
 
