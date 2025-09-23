4020a4b87e8fc63d029ce13692dcad9f017ed280
fushouhai <fushouhai@starmerx.com>
2025-09-23 14:36:23 +0800

    22. sum-of-multiples

--- src/lib.rs
+++ src/lib.rs
@@ -597,6 +597,24 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     stack.is_empty()
 }
 
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    use std::collections::HashSet;
+    let mut multiples = HashSet::new();
+
+    for &factor in factors {
+        if factor == 0 {
+            continue;
+        }
+        let mut multiple = factor;
+        while multiple < limit {
+            multiples.insert(multiple);
+            multiple += factor;
+        }
+    }
+
+    multiples.iter().sum()
+}
+
 
 #[cfg(test)]
 mod raindrops_tests {
